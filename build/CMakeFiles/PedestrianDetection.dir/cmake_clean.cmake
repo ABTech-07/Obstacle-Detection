c874ac586eb821f89cac3d615e0751001680ebb5
ABTech-07 <abandekar@clarku.edu>
2025-10-27 23:08:09 -0400

    pull from main

---
+++ build/CMakeFiles/PedestrianDetection.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PedestrianDetection.dir/src/main.cpp.o"
+  "CMakeFiles/PedestrianDetection.dir/src/main.cpp.o.d"
+  "PedestrianDetection"
+  "PedestrianDetection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PedestrianDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
